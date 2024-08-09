de123c6fdfda74366f5d9b00f06b85bac7391e30
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2024-08-09 04:06:59 -0300

    build(issue#2): Inserido Libs do grupo dev e criado tasks no pyproject.

--- backend/startup/settings.py
+++ backend/startup/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f^%6c2z%zs4(j0r0o%!+ygmbc@@*^^bzxlhfgda(4dixsg5ig6'
+SECRET_KEY = (
+    'django-insecure-f^%6c2z%zs4(j0r0o%!+ygmbc@@*^^bzxlhfgda(4dixsg5ig6'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
